e27a045a94df89312bd30ed7364d9cd3f72ea1b5
Karki-Biplab <k.nomem13@gmail.com>
2025-03-31 14:58:05 +0545

    Publishing Day 7 Practices

---
+++ Day 7/Hangman.py
@@ -0,0 +1,45 @@
+################# Day 7 ###################
+# #Hangman
+import random
+import hangmanwords
+chosen_word = random.choice(hangmanwords.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+import hangmanart
+print(hangmanart.logo)
+
+#print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print(f"You lose. The word was {chosen_word}")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+    print(hangmanart.stages[lives])
