90c38ffb378dc0f1d431f8110e66a9f452e3eb92
Karki-Biplab <k.nomem13@gmail.com>
2025-05-18 16:11:45 +0545

    Uploading day 24 practices

---
+++ Day 24(a)/main.py
@@ -0,0 +1,7 @@
+with open("C:\\Users\\User\\Desktop\\test.txt") as fi:
+    contents = fi.read()
+    print(contents)
+
+# # fi.close()
+# with open("test.txt", mode="a") as fi:
+#     fi.write("\n New akjsdjsjd m,dmsds nsdkjsd")
